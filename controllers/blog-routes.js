f1b88e6c73e78b64edca7fb4c788f32b8c00fd6d
Abigail Burtley <abigailtop95@yahoo.com>
2023-11-03 16:51:43 -0400

    add routes for login and dashboard page

--- controllers/blog-routes.js
+++ controllers/blog-routes.js
@@ -5,4 +5,11 @@ router.get('/', async (req, res) => {
   res.render('all');
 });
 
+router.get('/login', async (req, res) => { //check
+    res.render('login');
+})
+
+router.get('/dashboard', async (req, res) => { //check
+    res.render('dashboard');
+})
 module.exports = router;
