cabc088ffc1b498924304e50e9d92ab1d3945310
Abigail Burtley <abigailtop95@yahoo.com>
2023-11-03 00:16:25 -0400

    add session middleware to get session stored data

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers/blog-routes')); //check 
 
+
+const sess = {
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  };
+
+  
+app.use(session(sess));
+
 app.listen(PORT, () => {
     console.log('Now listening on http://localhost:' + PORT );
 })
