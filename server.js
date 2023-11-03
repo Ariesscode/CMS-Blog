b540704220a86f641f173ce897d676e4a9acada8
Abigail Burtley <abigailtop95@yahoo.com>
2023-11-03 01:28:07 -0400

    add pre temporary data post to all handlebar file

--- server.js
+++ server.js
@@ -7,24 +7,24 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/blog-routes')); //check 
+//app.use(require('./controllers/blog-routes')); //check 
 
 
-const sess = {
-    secret: SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-  };
+// const sess = {
+//     secret: SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: true,
+//   };
 
   
-app.use(session(sess));
+// app.use(session(sess));
 
 app.listen(PORT, () => {
     console.log('Now listening on http://localhost:' + PORT );
