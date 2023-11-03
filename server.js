bdb62648c3437d9585e776efa8cf0ea61656815e
Abigail Burtley <abigailtop95@yahoo.com>
2023-11-02 23:49:13 -0400

    require dependencies in server js file and assign a fall back PORT if enviornment variable is not accessed

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 require('dotenv').config();
+const express = require('express');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
