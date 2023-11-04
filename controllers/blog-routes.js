1be14acaa16c12df0b7ef6f6e221ca3622295b05
Abigail Burtley <abigailtop95@yahoo.com>
2023-11-03 20:29:36 -0400

    add routes for each page, dashboard, signup

--- controllers/blog-routes.js
+++ controllers/blog-routes.js
@@ -11,13 +11,20 @@ router.get('/', async (req, res) => {
 
 
 
-router.get('/login', async (req, res) => { //check
+router.get('/login', async (req, res) => { 
     res.render('login');
-})
+});
 
 router.get('/dashboard', async (req, res) => { //check
     res.render('dashboard');
-})
+});
+
+router.get('/home', async (req, res) => { //check
+    res.render('home');
+});
+
+
+
 module.exports = router;
 
 
