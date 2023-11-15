a5301b5a09d057db0cecb294e585334d32ec095a
Abigail Burtley <abigailtop95@yahoo.com>
2023-11-15 00:37:21 -0500

    blog post comments displayed using home routes

--- controllers/api/blog-routes.js
+++ controllers/api/blog-routes.js
@@ -7,35 +7,7 @@ const withAuth = require('../../utils/auth');
  //add auth after create app
 
 
-//  router.get('/', async (req, res) => {
-//   try {
-//       const blogData = await Blog.findAll({
-//           include: [{ model: Comment, include: User }] 
-//       });
-//       const blogPosts = blogData.map(post => post.get({ plain: true }));
-      
-//       return res.render('all', { blogPrePost, blogData, blogPosts });
-//   } catch (err) {
-//       console.error(err);
-//       res.status(500).json(err);
-//   }
-// });
-router.get('/', async (req, res) => {
-  try {
-      const blogPosts = await Blog.findAll({
-          include: [{ model: Comment }],
-      });
-
-      res.render('blog-posts', { blogPosts });
-  } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-
-
-
+ 
 router.post('/comment/:blog_id', withAuth, async (req, res) => {
     try {
      
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,24 +1,44 @@
-const router = require('express').Router();
-const blogPrePost = require('../pre-blogData');
-const Comment = require('../models/comment');
-const Blog = require('../models/blog');
-
-
-
-
-router.get('/', async (req, res) => {
-
-  const blogData = await Blog.findAll();
-  const blogPosts = blogData.map(post => post.get({plain: true}))
-  const commentData = await Comment.findAll();
-  const blogComments = commentData.map(comment => comment.get({plain: true}))
-
-  return res.render('all', { blogPrePost, blogData, blogPosts, blogComments });
+const router = require("express").Router();
+const blogPrePost = require("../pre-blogData");
+const Comment = require("../models/comment");
+const Blog = require("../models/blog");
+const User = require("../models/user");
+router.get("/", async (req, res) => {
+  const blogData = await Blog.findAll({
+    include: [
+      {
+        model: Comment,
+        include: [User],
+      },
+    ],
+  });
+  const blogPosts = blogData.map((post) => post.get({ plain: true }));
+  return res.render("all", { blogPrePost, blogData, blogPosts });
 });
-
-
-router.get('/login', async (req,res) => {
-  res.render('login');
+router.get("/login", async (req, res) => {
+  res.render("login");
 });
-
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
