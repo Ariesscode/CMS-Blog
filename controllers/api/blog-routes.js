9f1ed267b32fd76bb9b17180f88f599fac990369
Abigail Burtley <abigailtop95@yahoo.com>
2023-11-14 21:49:12 -0500

    push bug

--- controllers/api/blog-routes.js
+++ controllers/api/blog-routes.js
@@ -2,32 +2,59 @@ const router = require('express').Router();
 const blogPrePost = require('../../pre-blogData');
 const Comment = require('../../models/comment');
 const Blog = require('../../models/blog');
+const User = require('../../models/user');
 const withAuth = require('../../utils/auth');
  //add auth after create app
 
 
-router.get('/', async (req, res) => {
-
+router.get('/', async (req, res) => {  
+ 
     const blogData = await Blog.findAll();
     const blogPosts = blogData.map(post => post.get({plain: true}))
     const commentData = await Comment.findAll();
     const blogComments = commentData.map(comment => comment.get({plain: true}))
 
-    return res.render('all', { blogPrePost, blogData, blogPosts, blogComments });
+    return res.render('all', { blogPrePost, blogData, blogPosts, blogComments});
+});
+
+router.get('/', async (req, res) => {  
+  try {
+    const blogData = await Blog.findAll({
+      include: [
+        {
+          model: Comment,
+          include: User, 
+        },
+      ],
+    });
+
+    const blogPosts = blogData.map(post => post.get({ plain: true }));
+
+    return res.render('all', { blogPosts });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
 
+
+
+
+
   
-router.post('/:blog_id/comment', withAuth, async (req, res) => {
+router.post('/comment', withAuth, async (req, res) => {
     try {
-      // Ensure that req.session.user_id is available to associate the comment with the user
+     
       const newComment = await Comment.create({
         ...req.body,
         blog_id: req.params.blog_id,
-        user_id: req.session.user_id, // Assuming user_id is stored in the session during login
+        user_id: req.session.user_id,
+        comment: req.body.comment
       });
-  
       res.status(200).json(newComment);
+      
+  
     } catch (err) {
       console.error(err);
       res.status(500).json(err);
